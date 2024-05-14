d42f9ccd08503326bad6c32e18506f44a3fc0c71
VladVasili4 <dipol777@ya.ru>
2024-05-14 20:19:32 +0300

    Add files via upload

---
+++ While.py
@@ -0,0 +1,9 @@
+
+while True:
+    my_symbol1 = int(input('Enter anything digit \n', ))
+    if my_symbol1 > 0:
+        print('Positive')
+    else:
+        print('Negative')
+        break
+print('Good Buy')
