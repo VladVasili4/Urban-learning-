0ebf8d534917ddda966e43caff5f239057f99f1a
VladVasili4 <dipol777@ya.ru>
2024-05-16 12:34:45 +0300

    Add files via upload

---
+++ FirstFunction.py
@@ -0,0 +1,7 @@
+def say_hello(name):
+    twice_ = ('Hello ' + name + '\t') * 2
+    print(twice_)
+
+say_hello(input('Enter your name \n'))
+say_hello(input('Enter your Second name \n'))
+say_hello(input('Enter friends name with you \n'))
